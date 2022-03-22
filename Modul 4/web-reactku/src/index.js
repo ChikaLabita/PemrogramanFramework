89943a592359e2acb4b7c1ef690db82e3c472cc0
ChikaLabita <itaaa.c2@gmail.com>
2022-03-23 06:57:16 +0700

    Praktikum 1 : -

--- Modul 4/web-reactku/src/index.js
+++ Modul 4/web-reactku/src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+import BlogPost from "./Container/BlogPost/BlogPost"
+
+ReactDOM.render(<BlogPost />, document.getElementById('root'));
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
@@ -14,4 +18,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+*/
 reportWebVitals();
