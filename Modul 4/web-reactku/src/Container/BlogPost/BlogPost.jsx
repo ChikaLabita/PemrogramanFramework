5bd8816f1d4b82cc8e7e2038be85778ed5d030f7
ChikaLabita <itaaa.c2@gmail.com>
2022-03-23 22:37:28 +0700

    Praktikum

--- Modul 4/web-reactku/src/Container/BlogPost/BlogPost.jsx
+++ Modul 4/web-reactku/src/Container/BlogPost/BlogPost.jsx
@@ -1,11 +1,35 @@
 import React, {Component} from "react";
 import './BlogPost.css';
+import Post from "../../Component/BlogPost/Post";
+import { render } from "react-dom";
 
 class BlogPost extends Component{
+
+	 state = {
+        listArtikel: []
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(jsonHasilAmbilDariAPI => {
+                this.setState({
+                    listArtikel: jsonHasilAmbilDariAPI
+                })
+            })
+    }
+
 	render() {
 		return(
-			<p>Blog Artikel</p>
-			)
+			<div class="post-artikel">
+            <h2>Daftar artikel</h2>
+            {
+                this.state.listArtikel.map(artikel => {
+                    return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} />
+                })
+            }
+            </div>
+		)
 	}
 }
 
