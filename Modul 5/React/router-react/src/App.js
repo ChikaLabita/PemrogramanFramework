3ee17d9fa7ae811239049ad6cc40f7ba8e5e786e
chikaLabita <itaaa.c2@gmail.com>
2022-04-04 15:49:53 +0700

    Praktikum | | a. Basic Router

--- Modul 5/React/router-react/src/App.js
+++ Modul 5/React/router-react/src/App.js
@@ -1,8 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+/*import logo from './logo.svg';
+import './App.css';*/
 
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+export default function App() {
   return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+
+//function App() {
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +85,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+  );*/
+//}
