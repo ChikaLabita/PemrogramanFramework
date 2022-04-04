9816a471f00f7ee6c4427fcc42bc8fd95172e090
chikaLabita <itaaa.c2@gmail.com>
2022-04-04 16:11:11 +0700

    Praktikum | | b. URL Parameter

--- Modul 5/React/router-react/src/App.js
+++ Modul 5/React/router-react/src/App.js
@@ -1,5 +1,5 @@
 /*import logo from './logo.svg';
-import './App.css';*/
+import './App.css';
 
 import React from "react";
 import {
@@ -65,7 +65,53 @@ function Dashboard() {
     </div>
   );
 }
+*/
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <h2>Accounts</h2>
+        <ul>
+          <li>
+            <Link to="/netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/gmail">Gmail</Link>
+          </li>
+          <li>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/amazon">Amazon</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/:id" children={<Child />} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+function Child() {
+  let { id } = useParams();
+  return (
+    <div>
+      <h3>ID: {id} </h3>
+    </div>
+  )
+}
 
 //function App() {
   /*return (
