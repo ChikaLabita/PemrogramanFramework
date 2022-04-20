27450633e7e4c1f3dc4cae35a302bd9a4a788f4a
chikaLabita <itaaa.c2@gmail.com>
2022-04-21 02:20:44 +0700

    Modul 7 : HOC

--- Modul 5/React/router-react/src/App.js
+++ Modul 5/React/router-react/src/App.js
@@ -529,7 +529,498 @@ export default function App(){
     </div>
   );*/
 //}
-import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+  useHistory,
+  useLocation,
+  Redirect
+} from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import { Container } from 'react-bootstrap';
+import { Alert } from "react-bootstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import { Card } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { ListGroupItem } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+export default function AuthExample() {
+  return (
+    <Router>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/home">Istiqomah Store | | Halal</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home"><b>Home</b></Nav.Link>
+            <Nav.Link href="/private"><AuthButton /></Nav.Link>
+            <Nav.Link href="/orderprivate"><AuthButton2 /></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div>
+
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/private">
+            <ProtectedPage />
+          </PrivateRoute>
+          <PrivateRoute path="/orderprivate">
+            <ProtectedPage />
+          </PrivateRoute>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+  ordernow(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+};
+
+function AuthButton() {
+  let history = useHistory();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/link/" } };
+
+  return fakeAuth.isAuthenticated ? (
+    <b
+      onClick={() => {
+        fakeAuth.signout(() => history.push(from));
+      }}
+    >
+      Sign out
+    </b>
+  ) : (
+    <b>Products</b>
+  );
+}
+
+function AuthButton2() {
+  let history = useHistory();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/link/" } };
+
+  return fakeAuth.ordernow ? (
+    <b
+      onClick={() => {
+        fakeAuth.ordernow(() => history.get(from));
+      }}
+    >
+      Order Now
+    </b>
+  ) : (
+    <b>Order Now</b>
+  );
+}
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function Home() {
+  return (
+    <Container>
+      <Row className="my-5">
+      <h2>Selamat Datang di Toko Istiqomah Store ~ * </h2>
+      <text>Jl. Alianda No. 32 Kota Pasuruan  | Kode Pos: 67161 | E-mail: istiqomahstore@dummy.com</text>
+      <hr />
+      <br />
+      <h3>Best Sellers Toko Kami </h3>
+      <br />
+        <Col> <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/f580f8c707f11b79cd05b464b0fe90ab" />
+          <Card.Body>
+            <Card.Title>Bergo Instant</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://id-test-11.slatic.net/p/bf7baeb79c4e533da11b420c99d2c497.jpg" />
+          <Card.Body>
+            <Card.Title>Salmicraft</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/a7dce3d330816470903aaf3c23549124" />
+          <Card.Body>
+            <Card.Title>Aisyabana</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/c85dbc597c03996bd8a9dafc92ee8a52" />
+          <Card.Body>
+            <Card.Title>Diamon Khimar</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://i.pinimg.com/originals/1f/e6/16/1fe6168d9d05eadd1563e55b07502ca4.jpg" />
+          <Card.Body>
+            <Card.Title>One set Cadar</Card.Title>
+            <Card.Text><b>
+              IDR.88.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        </Col>
+        <Col> <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/f580f8c707f11b79cd05b464b0fe90ab" />
+          <Card.Body>
+            <Card.Title>Bergo Instant</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://id-test-11.slatic.net/p/bf7baeb79c4e533da11b420c99d2c497.jpg" />
+          <Card.Body>
+            <Card.Title>Salmicraft</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/a7dce3d330816470903aaf3c23549124" />
+          <Card.Body>
+            <Card.Title>Aisyabana</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/c85dbc597c03996bd8a9dafc92ee8a52" />
+          <Card.Body>
+            <Card.Title>Diamon Khimar</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://i.pinimg.com/originals/1f/e6/16/1fe6168d9d05eadd1563e55b07502ca4.jpg" />
+          <Card.Body>
+            <Card.Title>One set Cadar</Card.Title>
+            <Card.Text><b>
+              IDR.88.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        </Col>
+        <Col> <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/f580f8c707f11b79cd05b464b0fe90ab" />
+          <Card.Body>
+            <Card.Title>Bergo Instant</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://id-test-11.slatic.net/p/bf7baeb79c4e533da11b420c99d2c497.jpg" />
+          <Card.Body>
+            <Card.Title>Salmicraft</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/a7dce3d330816470903aaf3c23549124" />
+          <Card.Body>
+            <Card.Title>Aisyabana</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/c85dbc597c03996bd8a9dafc92ee8a52" />
+          <Card.Body>
+            <Card.Title>Diamon Khimar</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://i.pinimg.com/originals/1f/e6/16/1fe6168d9d05eadd1563e55b07502ca4.jpg" />
+          <Card.Body>
+            <Card.Title>One set Cadar</Card.Title>
+            <Card.Text><b>
+              IDR.88.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        </Col>
+        </Row>
+      </Container>
+  );
+}
+function ProtectedPage() {
+  return (
+    <Container>
+      <br />
+      <Row className="my-5">
+        <h1>Assalamualaikum, Customer! Selamat Belanja</h1>
+        <br />
+        <br />
+        <text>Eitss, Belanja seperlunya ya! Ingat apapun akan dihisab. Terima Kasih</text>
+        <hr />
+        <br />
+        <h2>List Products</h2>
+        <hr />
+      <Col> <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/f580f8c707f11b79cd05b464b0fe90ab" />
+          <Card.Body>
+            <Card.Title>Bergo Instant</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://id-test-11.slatic.net/p/bf7baeb79c4e533da11b420c99d2c497.jpg" />
+          <Card.Body>
+            <Card.Title>Salmicraft</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/a7dce3d330816470903aaf3c23549124" />
+          <Card.Body>
+            <Card.Title>Aisyabana</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/c85dbc597c03996bd8a9dafc92ee8a52" />
+          <Card.Body>
+            <Card.Title>Diamon Khimar</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://i.pinimg.com/originals/1f/e6/16/1fe6168d9d05eadd1563e55b07502ca4.jpg" />
+          <Card.Body>
+            <Card.Title>One set Cadar</Card.Title>
+            <Card.Text><b>
+              IDR.88.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        </Col>
+        <Col> <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/f580f8c707f11b79cd05b464b0fe90ab" />
+          <Card.Body>
+            <Card.Title>Bergo Instant</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://id-test-11.slatic.net/p/bf7baeb79c4e533da11b420c99d2c497.jpg" />
+          <Card.Body>
+            <Card.Title>Salmicraft</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/a7dce3d330816470903aaf3c23549124" />
+          <Card.Body>
+            <Card.Title>Aisyabana</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/c85dbc597c03996bd8a9dafc92ee8a52" />
+          <Card.Body>
+            <Card.Title>Diamon Khimar</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://i.pinimg.com/originals/1f/e6/16/1fe6168d9d05eadd1563e55b07502ca4.jpg" />
+          <Card.Body>
+            <Card.Title>One set Cadar</Card.Title>
+            <Card.Text><b>
+              IDR.88.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        </Col>
+        <Col> <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/f580f8c707f11b79cd05b464b0fe90ab" />
+          <Card.Body>
+            <Card.Title>Bergo Instant</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://id-test-11.slatic.net/p/bf7baeb79c4e533da11b420c99d2c497.jpg" />
+          <Card.Body>
+            <Card.Title>Salmicraft</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/a7dce3d330816470903aaf3c23549124" />
+          <Card.Body>
+            <Card.Title>Aisyabana</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://cf.shopee.co.id/file/c85dbc597c03996bd8a9dafc92ee8a52" />
+          <Card.Body>
+            <Card.Title>Diamon Khimar</Card.Title>
+            <Card.Text><b>
+              IDR.28.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://i.pinimg.com/originals/1f/e6/16/1fe6168d9d05eadd1563e55b07502ca4.jpg" />
+          <Card.Body>
+            <Card.Title>One set Cadar</Card.Title>
+            <Card.Text><b>
+              IDR.88.000</b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/link/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <Alert variant="warning">
+        <Alert.Heading><center>Ops.. Maaf. Untuk Melihat Products dan Order di toko kami, Silahkan Login Dahulu!</center></Alert.Heading>
+        <center><Button variant="danger" onClick={login}>Log in</Button></center>
+      </Alert>
+    </div>
+  );
+}
+/*import './App.css';
  import Navbar from './components/Navbar';
 
  function App() {
@@ -538,4 +1029,4 @@ import './App.css';
  );
  }
 
- export default App;
+ export default App;*/
